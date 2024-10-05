f2f7bc940c644a96fbe55bf8249f87c7adea994a
Gvelasquez100 <112146352+Gvelasquez100@users.noreply.github.com>
2024-10-05 11:37:39 -0600

    Update config_bd.php

--- appLab/config_bd.php
+++ appLab/config_bd.php
@@ -1,7 +1,7 @@
 <?php
     
     // Variables de la conexion a la DB
-    $mysqli = new mysqli("autorack.proxy.rlwy.net:27254","root","aboRYhGGmKPgbMxJVXXPGxSvfzAWCOYg","db_gestor1"); //pruebas
+    $mysqli = new mysqli("autorack.proxy.rlwy.net:27254","root","aboRYhGGmKPgbMxJVXXPGxSvfzAWCOYg","db_muestra"); //pruebas
     //$mysqli = new mysqli("monorail.proxy.rlwy.net:37979","root","QYknGkzhCrHRctLkLrnLyeZmjXdBssGR","db_gestor"); //servidor
     
     // Comprobamos la conexion
