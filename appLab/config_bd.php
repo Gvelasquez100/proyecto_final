633bb55dcb0b0d4d42f2c4aa475cfc242a41ea43
Gvelasquez100 <112146352+Gvelasquez100@users.noreply.github.com>
2024-10-19 23:56:44 -0600

    Update config_bd.php

--- appLab/config_bd.php
+++ appLab/config_bd.php
@@ -1,12 +1,11 @@
 <?php
     
-
     $mysqli = new mysqli("autorack.proxy.rlwy.net:57379", "root", "kaGqWhBngHXlhCoRjWvTCvPGVTXeMcEk", "db_muestras");  
 
     if($mysqli->connect_errno) {
         die("Fallo la conexion");
     } else {
-        //echo "Conexion exitosa";
+
     }
 
 ?>
