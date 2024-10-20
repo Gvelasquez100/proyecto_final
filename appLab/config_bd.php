2bedf11fd82d5b9c4303dcda99b84bdaf78f4dc1
Gvelasquez100 <112146352+Gvelasquez100@users.noreply.github.com>
2024-10-19 23:29:24 -0600

    Update config_bd.php

--- appLab/config_bd.php
+++ appLab/config_bd.php
@@ -1,10 +1,8 @@
 <?php
     
-    // Variables de la conexion a la DB
-    //$mysqli = new mysqli("127.0.0.1:3306","root","root","db_muestras"); //pruebas
-    //$mysqli = new mysqli("monorail.proxy.rlwy.net:37979","root","QYknGkzhCrHRctLkLrnLyeZmjXdBssGR","db_gestor"); //servidor
+
     $mysqli = new mysqli("autorack.proxy.rlwy.net:57379", "root", "kaGqWhBngHXlhCoRjWvTCvPGVTXeMcEk", "db_muestras");  
-    // Comprobamos la conexion
+
     if($mysqli->connect_errno) {
         die("Fallo la conexion");
     } else {
